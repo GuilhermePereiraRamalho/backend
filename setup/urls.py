2eac12958f4ea52b9f5569a6c2bfe1f0c6ece646
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2024-02-12 12:56:37 +0100

    Ajustando configuracoes

--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,14 @@
-from django.urls import include, path
+from django.contrib import admin
+from django.urls import path, include
 from rest_framework import routers
-
-from tutorial.quickstart import views
+from backend import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
